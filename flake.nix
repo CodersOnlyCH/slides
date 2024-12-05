fa469bdc41ae5664fc5d5a9a715c1f1370dd7247
Pablo Escodebar <escodebar@gmail.com>
2024-12-05 20:11:16 +0100

    Create a default shell with reveal-md

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem = {pkgs, ...}: {
+        devShells = {
+          default = pkgs.mkShell {};
+          presentation = pkgs.mkShell {
+            packages = with pkgs; [
+              reveal-md
+            ];
+          };
+        };
+      };
+    };
+}
