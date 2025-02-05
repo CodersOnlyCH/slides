59f5ce8fd2720fab0fa71af9486866ded85297a2
Pablo Escodebar <escodebar@gmail.com>
2025-02-05 14:30:17 +0100

    Move general assets to assets package

--- packages.nix
+++ packages.nix
@@ -1,4 +1,20 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  stdenv,
+  ...
+}: let
+  assets = stdenv.mkDerivation {
+    pname = "assets";
+    version = "2025";
+    src = ./assets;
+    buildPhase = '''';
+    installPhase = ''
+      mkdir -p $out
+      cp -r * $out
+    '';
+  };
+in {
+  inherit assets;
   inherit (pkgs.callPackage ./gv/packages.nix {}) gv-2025;
   inherit (pkgs.callPackage ./meetups/nix/packages.nix {}) nix-meetup-2024-11-12;
   inherit (pkgs.callPackage ./marp/themes/packages.nix {}) coders-only-marp-theme;
